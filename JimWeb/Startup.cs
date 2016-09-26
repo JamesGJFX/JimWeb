1b735c17e9494297da650d7885052fdddd152b8f
James <James@JAMES-DESKTOP>
2016-09-26 22:05:50 +0100

    Add project files.

---
+++ JimWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JimWeb.Startup))]
+namespace JimWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
